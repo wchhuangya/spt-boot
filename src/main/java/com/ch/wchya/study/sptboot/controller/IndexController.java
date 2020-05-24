dafa812b06281f5762bf5f4ef6de84fa7167e52c
wchya <wchhuangyawork@163.com>
2020-05-24 18:48:35 +0800

    添加 bootstrap；制作 index.html；

---
+++ src/main/java/com/ch/wchya/study/sptboot/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.ch.wchya.study.sptboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @program: spt-boot
+ * @description:
+ * @author: 王超
+ * @create: 2020-05-23 23:17
+ **/
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "index";
+    }
+}
